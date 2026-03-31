8eb4fa791269ed62c70cf53a7accf18a17cac348
SohailaAbdElazeem <abdelazeemsohaila@gmail.com>
2026-03-31 22:01:56 +0200

    Add drag and touch support for horizontal slider

--- app.js
+++ app.js
@@ -220,3 +220,54 @@ form.addEventListener('submit', function(e){
   window.location.href = `mailto:panda-oracle@bo-chat.space?subject=${subject}&body=${body}`;
 });
  
+
+//  Rating
+ document.addEventListener('DOMContentLoaded', () => {
+  const sliders = document.querySelectorAll('.rate');
+  sliders.forEach(slider => {
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+    slider.addEventListener('mousedown', e => {
+      isDown = true;
+      slider.classList.add('active');
+      startX = e.pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
+    });
+
+    slider.addEventListener('mouseleave', () => {
+      isDown = false;
+      slider.classList.remove('active');
+    });
+
+    slider.addEventListener('mouseup', () => {
+      isDown = false;
+      slider.classList.remove('active');
+    });
+
+    slider.addEventListener('mousemove', e => {
+      if(!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - slider.offsetLeft;
+      const walk = (x - startX) * 2; 
+      slider.scrollLeft = scrollLeft - walk;
+    });
+    slider.addEventListener('touchstart', e => {
+      isDown = true;
+      startX = e.touches[0].pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
+    });
+
+    slider.addEventListener('touchmove', e => {
+      if(!isDown) return;
+      const x = e.touches[0].pageX - slider.offsetLeft;
+      const walk = (x - startX) * 2;
+      slider.scrollLeft = scrollLeft - walk;
+    });
+
+    slider.addEventListener('touchend', () => {
+      isDown = false;
+    });
+  });
+
+});
